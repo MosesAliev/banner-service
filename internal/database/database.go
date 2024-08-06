85fbb7c3b62837785dbc29138dd0bee057e2a566
Муса Алиев <73427037+MaxBron@users.noreply.github.com>
2024-08-06 20:15:56 +0300

    added dockerfile

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"banner-service/internal/models"
+	"fmt"
 	"log"
 	"os"
 
@@ -21,7 +22,14 @@ func ConnectDB() {
 	// Имя пользователя базы данных, пароль и имя базы данных
 	// берутся из переменных окружения,
 	// они описаны в файле .env
-	dsn := "host=localhost user=postgres password=5280 dbname=banner-service port=5432 sslmode=disable TimeZone=Europe/Moscow"
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Moscow",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
 	// создаём подключение к базе данных.
 	// В &gorm.Config настраивается логер,
 	// который будет сохранять информацию
